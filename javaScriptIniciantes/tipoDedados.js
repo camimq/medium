c783587de5b2f3db62f26d9052fa3eb3a9650add
Camila Queiroz <camimq85@gmail.com>
2021-09-22 18:14:03 -0300

    adiciona exemplos de código para o artigo JavaScript para iniciantes.

---
+++ javaScriptIniciantes/tipoDedados.js
@@ -0,0 +1,21 @@
+var nome = 'Camila';
+var idade = '36';
+var possuiFaculdade = true;
+var time;
+var comida = null;
+var simbolo = Symbol();
+var novoObjeto = {}
+
+console.log(typeof nome); // String
+console.log(typeof idade); // Number
+console.log(typeof possuiFaculdade); // Boolean
+console.log(typeof time); // Undefined
+console.log(typeof comida); // Null
+console.log(typeof simbolo); // Symbol
+console.log(typeof novoObjeto); // Object
+
+//-----------------------------------------------------
+
+var naoDefinido;
+
+console.log(typeof naoDefinido) // undefined
