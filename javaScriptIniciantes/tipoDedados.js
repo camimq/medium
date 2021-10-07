731f7cf291e5974a43d0c7db94b18700e9da5fc3
Camila Queiroz <camimq85@gmail.com>
2021-10-07 10:17:32 -0300

    Insere exemplo de código.

--- javaScriptIniciantes/tipoDedados.js
+++ javaScriptIniciantes/tipoDedados.js
@@ -42,3 +42,14 @@ console.log('num'/2); // => NaN
     nome = 'João';
 
     console.log(nome); // => João
+
+// -----------------------------------------------------
+
+console.log(Boolean('Hi')); // => true
+console.log(Boolean('')); // => false
+console.log(Boolean(20)); // => true
+console.log(Boolean(Infinity)); // => true
+console.log(Boolean(0)); // false
+
+console.log(Boolean({foo:100})); // true on non-empty object
+console.log(Boolean(null)); // false
